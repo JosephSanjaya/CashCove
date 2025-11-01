760d370da047fc9661ef5e735c9711635a5d0fd9
namini40 <namini40@gmail.com>
2025-11-01 17:27:21 +0330

    feat(app): initialize Koin with LoggerModule and NetworkModule

--- app/src/main/java/com/cashcove/app/CashCoveApp.kt
+++ app/src/main/java/com/cashcove/app/CashCoveApp.kt
@@ -1,5 +1,23 @@
 package com.cashcove.app
 
 import android.app.Application
+import com.cashcove.core.di.LoggerModule
+import com.cashcove.core.di.NetworkModule
+import com.tencent.mmkv.BuildConfig
+import org.koin.core.context.startKoin
+import org.koin.ksp.generated.module
 
-class CashCoveApp : Application()
+class CashCoveApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            if (BuildConfig.DEBUG) {
+                // setup logger
+            }
+            modules(
+                LoggerModule.module,
+                NetworkModule.module,
+            )
+        }
+    }
+}
