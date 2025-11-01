ce039bba41ff0169d5de44adc600b62d2d874b34
namini40 <namini40@gmail.com>
2025-11-01 17:27:16 +0330

    refactor(core): migrate NetworkModule from Hilt to Koin

--- core/src/main/java/com/cashcove/core/di/NetworkModule.kt
+++ core/src/main/java/com/cashcove/core/di/NetworkModule.kt
@@ -1,49 +1,41 @@
 package com.cashcove.core.di
-//
-// @Module
-// @InstallIn(SingletonComponent::class)
-// object NetworkModule {
-//    @Provides
-//    @Singleton
-//    fun provideHttpLoggingInterceptor(networkLoggingInterceptor: NetworkLoggingInterceptor): HttpLoggingInterceptor =
-//        networkLoggingInterceptor.create()
-//
-//    @Provides
-//    @Singleton
-//    fun provideOkHttpClient(
-//        requestInterceptor: RequestInterceptor,
-//        loggingInterceptor: HttpLoggingInterceptor,
-//    ): OkHttpClient =
-//        OkHttpClient
-//            .Builder()
-//            .addInterceptor(requestInterceptor)
-//            .addInterceptor(loggingInterceptor)
-//            .connectTimeout(AppConstants.Times.GENERAL_REQUEST_TIMEOUT, TimeUnit.SECONDS)
-//            .readTimeout(AppConstants.Times.REQUEST_READ_TIMEOUT, TimeUnit.SECONDS)
-//            .writeTimeout(AppConstants.Times.REQUEST_WRITE_TIMEOUT, TimeUnit.SECONDS)
-//            .retryOnConnectionFailure(true)
-//            .build()
-//
-//    @Provides
-//    @Singleton
-//    fun provideGson(): Gson =
-//        GsonBuilder()
-//            .create()
-//
-//    @Provides
-//    @Singleton
-//    fun provideRetrofit(
-//        okHttpClient: OkHttpClient,
-//        gson: Gson,
-//    ): Retrofit =
-//        Retrofit
-//            .Builder()
-//            .baseUrl(AppConstants.Network.BASE_URL)
-//            .client(okHttpClient)
-//            .addConverterFactory(GsonConverterFactory.create(gson))
-//            .build()
-//
-//    @Provides
-//    @Singleton
-//    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
-// }
+
+import com.cashcove.core.common.AppConstants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
+import java.util.concurrent.TimeUnit
+
+@Module
+object NetworkModule {
+
+    @Single
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .connectTimeout(AppConstants.Times.GENERAL_REQUEST_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(AppConstants.Times.REQUEST_READ_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(AppConstants.Times.REQUEST_WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+
+    @Single
+    fun provideGson(): Gson =
+        GsonBuilder()
+            .create()
+
+    @Single
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gson: Gson,
+    ): Retrofit =
+        Retrofit
+            .Builder()
+            .baseUrl(AppConstants.Network.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+}
