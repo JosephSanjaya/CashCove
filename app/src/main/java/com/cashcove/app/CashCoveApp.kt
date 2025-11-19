94b6985cf800440e0cd3569a11c91dc8145037b8
namini40 <namini40@gmail.com>
2025-11-19 12:48:14 +0330

    fix(app,authentication): add module to load viewmodel di on app startup

--- app/src/main/java/com/cashcove/app/CashCoveApp.kt
+++ app/src/main/java/com/cashcove/app/CashCoveApp.kt
@@ -3,12 +3,18 @@ package com.cashcove.app
 import android.app.Application
 import com.cashcove.app.di.AppModule
 import com.cashcove.core.di.DiManager
+import com.cashcove.features.authentication.data.di.AuthenticationModule
 import org.koin.core.context.loadKoinModules
 
 class CashCoveApp : Application() {
     override fun onCreate() {
         super.onCreate()
         DiManager.startInitModules(this)
-        loadKoinModules(AppModule)
+        loadKoinModules(
+            listOf(
+                AppModule,
+                AuthenticationModule,
+            )
+        )
     }
 }
