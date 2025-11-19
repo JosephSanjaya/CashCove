6f518057c2f788fb0201c8cd92e01483bd45ca3c
namini40 <namini40@gmail.com>
2025-11-19 16:54:47 +0330

    feat(authentication): update Login

--- features/authentication/src/main/java/com/cashcove/features/authentication/presentation/login/LoginIntent.kt
+++ features/authentication/src/main/java/com/cashcove/features/authentication/presentation/login/LoginIntent.kt
@@ -3,4 +3,6 @@ package com.cashcove.features.authentication.presentation.login
 sealed interface LoginIntent {
     data object NavigateToRegister : LoginIntent
     data object NavigateToMain : LoginIntent
+    data class UpdatePhoneNumber(val phoneNumber: String) : LoginIntent
+    data class SendOtp(val phoneNumber: String) : LoginIntent
 }
