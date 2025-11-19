531bd1d60227025a9e2fc33eced7c37ba963af32
namini40 <namini40@gmail.com>
2025-11-19 12:50:32 +0330

    fix(authentication): add view model di

--- features/authentication/src/main/java/com/cashcove/features/authentication/data/di/AuthenticationModule.kt
+++ features/authentication/src/main/java/com/cashcove/features/authentication/data/di/AuthenticationModule.kt
@@ -1,11 +1,15 @@
 package com.cashcove.features.authentication.data.di
 
 import com.cashcove.features.authentication.presentation.login.LoginViewModel
+import com.cashcove.features.authentication.presentation.onbording.OnBoardingViewModel
+import com.cashcove.features.authentication.presentation.register.RegisterViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val AuthenticationModule = module {
     viewModel {
         LoginViewModel(Unit)
+        RegisterViewModel(Unit)
+        OnBoardingViewModel(Unit)
     }
 }
