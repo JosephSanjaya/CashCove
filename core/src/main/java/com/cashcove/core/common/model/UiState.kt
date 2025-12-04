cdf7291c479a9c1bd51dc5c8e8daa9de6360d029
namini40 <namini40@gmail.com>
2025-12-05 01:56:58 +0330

    refactor(core): make Success in UiState more consistent

--- core/src/main/java/com/cashcove/core/common/model/UiState.kt
+++ core/src/main/java/com/cashcove/core/common/model/UiState.kt
@@ -5,7 +5,7 @@ package com.cashcove.core.common.model
  * Using sealed interface allows for exhaustive when branches.
  */
 sealed interface UiState<out T> {
-    data class Success<T>(val data: T) : UiState<T>
+    data class Success<out T>(val data: T) : UiState<T>
     data class Error(val message: String? = null) : UiState<Nothing>
     data object Loading : UiState<Nothing>
     data object Idle : UiState<Nothing>
